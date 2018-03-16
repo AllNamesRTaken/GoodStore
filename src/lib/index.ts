84525bd7cf43bb9985dd818093bf3cf940eed3b8
Joel Brage <joel.brage@zafari.se>
2018-03-16 17:48:19 +0100

    added CallerHandle and CallerInternal to dts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
+export { CallerHandle as CallerHandle } from "./CallerHandle";
+export { CallerInternal as CallerInternal } from "./CallerInternal";
 export { DataStore as DataStore } from "./DataStore";
 export { DataStoreConsumable as DataStoreConsumable } from "./DataStoreConsumable";
 export { DataStoreRequestDto as DataStoreRequestDto } from "./Dto/DataStoreRequestDto";
